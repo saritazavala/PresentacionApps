22d2f71635beaea5e48458bc4754bd69dcc0a003
saritazavala <42972998+saritazavala@users.noreply.github.com>
2019-05-14 16:17:10 -0600

    Comments

--- SchoolPlanner-master/app/src/main/java/schmitt_florian/schoolplanner/logic/Settings.java
+++ SchoolPlanner-master/app/src/main/java/schmitt_florian/schoolplanner/logic/Settings.java
@@ -4,64 +4,35 @@ package schmitt_florian.schoolplanner.logic;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-/**
- * Settings class represents all settings in the app. Settings are saved in {@link SharedPreferences}.
- * Use Factory Method {@link Settings#getInstance(Context)} to get current valid {@link Settings}.
- * Use {@link Settings#saveSettings()} to save this {@link Settings}-Object. Note that their is only
- * one {@link Settings}-Object saved in {@link SharedPreferences} at the same time.
- */
+
 public class Settings {
-    /**
-     * represents DD.MM.YYYY date format
-     */
+
     public static final String DATE_FORMAT_DDMMYYYY = "DD.MM.YYYY";
-    /**
-     * represents MM.DD.YYYY date format
-     */
+
     public static final String DATE_FORMAT_MMDDYYYY = "MM.DD.YYYY";
-    /**
-     * represents YYYY.MM.DD date format
-     */
+
     public static final String DATE_FORMAT_YYYYMMDD = "YYYY.MM.DD";
 
-    /**
-     * represents HH:MM date format
-     */
+
     public static final String TIME_FORMAT_HHMM = "HH:MM";
 
 
-    /**
-     * the key under which date format is saved to {@link SharedPreferences}
-     */
+
     private static final String DATE_FORMAT = "dateFormat";
-    /**
-     * the key under which periodsAtDay are saved to {@link SharedPreferences}
-     */
+ 
     private static final String PERIODS_AT_DAY = "periodsAtDay";
 
 
-    /**
-     * the context of the application
-     */
+
     private final Context context;
-    /**
-     * the date format used in the app. One of {@link Settings#DATE_FORMAT_DDMMYYYY} ,
-     * {@link Settings#DATE_FORMAT_MMDDYYYY} , {@link Settings#DATE_FORMAT_YYYYMMDD}
-     */
+
     private String activeDateFormat;
 
-    /**
-     * number of periods at each day
-     */
+
     private int periodsAtDay;
 
 
-    /**
-     * Factory method used to create a new {@link Settings} Object with current valid settings
-     *
-     * @param context the context of the application
-     * @return {@link Settings} Object with current valid settings
-     */
+
     public static Settings getInstance(Context context) {
         SharedPreferences preferences = context.getSharedPreferences(context.getApplicationContext().toString(), Context.MODE_PRIVATE);
 
@@ -71,23 +42,12 @@ public class Settings {
         );
     }
 
-    /**
-     * standard c'tor, consider using of {@link Settings#getInstance(Context)} for current app settings
-     *
-     * @param context          context of application
-     * @param activeDateFormat One of {@link Settings#DATE_FORMAT_DDMMYYYY} ,
-     *                         {@link Settings#DATE_FORMAT_MMDDYYYY} , {@link Settings#DATE_FORMAT_YYYYMMDD}
-     * @param periodsAtDay     number of periods at each day
-     */
     private Settings(Context context, String activeDateFormat, int periodsAtDay) {
         this.context = context;
         this.activeDateFormat = activeDateFormat;
         this.periodsAtDay = periodsAtDay;
     }
 
-    /**
-     * saves the current {@link Settings} to {@link SharedPreferences}, overwrites old settings
-     */
     public void saveSettings() {
         SharedPreferences.Editor preferences = context.getSharedPreferences(context.getApplicationContext().toString(), Context.MODE_PRIVATE).edit();
 
@@ -97,46 +57,25 @@ public class Settings {
         preferences.apply();
     }
 
-    //region getter & setter
 
-    /**
-     * gets the date format used in the app
-     *
-     * @return One of {@link Settings#DATE_FORMAT_DDMMYYYY} ,
-     * {@link Settings#DATE_FORMAT_MMDDYYYY} , {@link Settings#DATE_FORMAT_YYYYMMDD}
-     */
     public String getActiveDateFormat() {
         return activeDateFormat;
     }
 
-    /**
-     * sets the date format used in the app
-     *
-     * @param activeDateFormat One of {@link Settings#DATE_FORMAT_DDMMYYYY} ,
-     *                         {@link Settings#DATE_FORMAT_MMDDYYYY} , {@link Settings#DATE_FORMAT_YYYYMMDD}
-     */
+
     public void setActiveDateFormat(String activeDateFormat) {
         this.activeDateFormat = activeDateFormat;
     }
 
-    /**
-     * gets the number of periods at each day
-     *
-     * @return number of periods at each day
-     */
+
     public int getPeriodsAtDay() {
         return periodsAtDay;
     }
 
-    /**
-     * sets the number of periods at each day
-     *
-     * @param periodsAtDay number of periods at each day
-     */
     public void setPeriodsAtDay(int periodsAtDay) {
         this.periodsAtDay = periodsAtDay;
     }
-    //endregion
+
 
 
 }
